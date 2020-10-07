6642bc3ade9df1f50cda1233976ada28c5395273
Matija Osrecki <matija.osrecki@gmail.com>
2020-10-07 12:46:33 +0200

    added btn and coded seat selection :fire:

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -1,9 +1,39 @@
-const select = document.querySelector('select');
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied');
+const count = document.querySelector('#count');
+const total = document.querySelector('#total');
+const movieSelect = document.querySelector('#movie');
 
-// select.addEventListener('click', () => {
-//     select.classList.toggle('opened')
-// })
+let ticketPrice = +movieSelect.value;
 
-// select.addEventListener('change', () => {
-//     select.classList.toggle('opened')
-// })
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+
+// Seat click event
+container.addEventListener('click', e => {
+    if (
+        e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+
+})
+
+// not performant enough
+// seats.forEach(seat => seat.addEventListener('click', () => {
+//     seat.classList.toggle('selected');
+// }))
