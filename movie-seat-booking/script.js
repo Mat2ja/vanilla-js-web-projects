1b4780b3d97e80f871ca2756549428fbd6791716
Matija Osrecki <matija.osrecki@gmail.com>
2020-10-07 12:00:00 +0200

    built movie seat booking layout :fire:

---
+++ movie-seat-booking/script.js
@@ -0,0 +1,9 @@
+const select = document.querySelector('select');
+
+// select.addEventListener('click', () => {
+//     select.classList.toggle('opened')
+// })
+
+// select.addEventListener('change', () => {
+//     select.classList.toggle('opened')
+// })
